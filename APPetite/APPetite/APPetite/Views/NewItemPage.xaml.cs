2e1c6be5ef8e8794cbca6e64e90adeb5717dc444
YoRHa <snowline2015@gmail.com>
2021-11-05 09:36:13 +0700

    temp

---
+++ APPetite/APPetite/APPetite/Views/NewItemPage.xaml.cs
@@ -0,0 +1,21 @@
+﻿using APPetite.Models;
+using APPetite.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace APPetite.Views
+{
+    public partial class NewItemPage : ContentPage
+    {
+        public Item Item { get; set; }
+
+        public NewItemPage()
+        {
+            InitializeComponent();
+            BindingContext = new NewItemViewModel();
+        }
+    }
+}
